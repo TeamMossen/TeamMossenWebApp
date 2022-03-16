cf135a81f68077076af3546c868a504c74711cd1
Kyemate <henke@mdpn.net>
2022-03-16 18:42:54 +0100

    t

---
+++ server/src/Database/user.mjs
@@ -0,0 +1,14 @@
+import { notionPeoplesDatabaseId, notion } from '../notion.js'
+
+async function GetUserById(id) {
+    const response = await notion.databases.query({
+        database_id: notionPeoplesDatabaseId,
+    });
+    const person = response.results.filter(x => x.properties.Id.rich_text[0].plain_text == id).map(x => x.properties);
+    console.log(person);
+
+}
+
+export {
+    GetUserById
+}
