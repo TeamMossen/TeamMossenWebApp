1372ee8e1204138bce6921061f52f1496c4dd1fd
alexmar000 <alex.olave@inteheltfel.se>
2022-03-20 23:31:31 +0100

    hh

---
+++ client/src/pages/ManagerProjects.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+import ManagerProjectTable from '../components/ManagerProjectTable';
+import TimereportInput from '../components/TimereportInput';
+
+export default class ManagerProjects extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fetchedProjects: [],
+      projectsFetched: false
+    };
+  }
+  componentDidMount(){
+    const token = localStorage.getItem('token')
+
+    fetch("http://localhost:8000/activeProjects", {
+      method: 'GET',
+      headers: new Headers({
+        'Authorization': token,
+        'Content-Type': 'application/json'
+      })
+    })
+    .then((response) => response.json())
+    .then((data) => { 
+      this.setState({
+      fetchedProjects: data,
+      projectsFetched: true
+    });
+  });
+    
+  }
+  render() {
+
+    if (!this.state.projectsFetched)
+    {
+        return (
+            <h2>Loading...</h2>
+        )
+    }
+    if (this.state.projectsFetched)
+    {
+      return(
+        <>
+         
+          <div className='background-div'>
+            <img className='ica-pic' src={require("../static/images/icastampel.jpg")} alt="bild på stämpelklocka"></img>
+          </div>
+          
+            <img className="myPic" src={require("../static/images/clock.png")} alt="En klocka" />
+          
+          <TimereportInput props={this.state.fetchedProjects}/>
+          <ManagerProjectTable props={this.state.fetchedProjects} />
+         
+          
+        </>
+      )
+    }
+  }
+}
