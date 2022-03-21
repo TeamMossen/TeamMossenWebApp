faffa02ab6092af0d357617a0273b60c17e808f3
alexmar000 <alex.olave@inteheltfel.se>
2022-03-22 00:23:15 +0100

    hh

--- client/src/components/ManagerProjectTableRow.jsx
+++ client/src/components/ManagerProjectTableRow.jsx
@@ -1,17 +1,21 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 export default function ManagerProjectTableRow( props ) {
     const backgroundColor = props.proj['Hours left'].formula.number < 0 ? 'red' : 'none';
     const timeWarning = { 'backgroundColor' : backgroundColor}
+    let modifyTitleWarning = props.proj['Hours left'].formula.number < 0;
 
+    useEffect(() => {    if (modifyTitleWarning) { document.getElementById("projectTitle").innerHTML = document.getElementById("projectTitle").innerHTML + ' ❗️'} });
+    
     return (
     <tr>
-        <td>{props.proj.Projectname.title[0].plain_text}</td>
+        <td id="projectTitle">{props.proj.Projectname.title[0].plain_text}</td>
         <td>{props.proj.Status.select.name}</td>
         <td>{props.proj.Hours.number}</td>
         <td>{props.proj['Worked hours'].rollup.number}</td>
-        <td style= {timeWarning}>{props.proj['Hours left'].formula.number}</td>
+        <td style= {timeWarning} id="hours-left">{props.proj['Hours left'].formula.number}</td>
         <td>{props.proj.Timespan.date.start} - {props.proj.Timespan.date.end}</td>
+        {/* {document.getElementById("hours-left").innerHTML = hoursLeftTextWarning} */}
     </tr>
   );
 }
