5194f0bd9696f622441e27c724df1f77d1624170
alexmar000 <alex.olave@inteheltfel.se>
2022-03-23 00:21:55 +0100

    hej

--- client/src/components/User/ManagerProjectTableRow.jsx
+++ client/src/components/User/ManagerProjectTableRow.jsx
@@ -1,13 +1,23 @@
 import React, {useEffect, useState} from 'react';
 
 export default function ManagerProjectTableRow(props) {
+  console.log(props);
   let title = props.proj.Projectname.title[0].plain_text;
+  let reportsOutsideTimespan = props.proj.ReportTimes.rollup.array;
   const backgroundColor =
     props.proj["Hours left"].formula.number < 0 ? "red" : "none";
   const timeWarning = { backgroundColor: backgroundColor };
+  let outsideSpanWarning = { backgroundColor: "none" };
   let modifyTitleWarning = props.proj["Hours left"].formula.number < 0;
-  if (modifyTitleWarning) { title += " ❗️"; }
-  
+
+  reportsOutsideTimespan.every((report) => {
+    if (report.formula.string == "Fail") {
+      title += " ❗️";
+      outsideSpanWarning.backgroundColor = "red";
+      return;
+    }
+  });
+
   return (
     <tr>
       <td id="projectTitle">{title}</td>
@@ -17,7 +27,7 @@ export default function ManagerProjectTableRow(props) {
       <td style={timeWarning} id="hours-left">
         {props.proj["Hours left"].formula.number}
       </td>
-      <td>
+      <td style={outsideSpanWarning}>
         {props.proj.Timespan.date.start} - {props.proj.Timespan.date.end}
       </td>
       {/* {document.getElementById("hours-left").innerHTML = hoursLeftTextWarning} */}
