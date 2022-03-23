e28dd329b7010615ed17920f2a9e743eb78eb9b6
Kyemate <henke@mdpn.net>
2022-03-24 00:16:48 +0100

    skål notion

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,7 @@ import express from 'express'; //import express
 import cors from 'cors';
 import { userRouter } from './Routes/user.mjs';
 import { loginRouter } from './Routes/login.mjs';
+import { bossRouter } from './Routes/boss.mjs';
 
 //const routes = require('./Routes/user'); // import the routes
 
@@ -15,7 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/', userRouter, loginRouter); //to use the routes
+app.use('/', userRouter, loginRouter, bossRouter); //to use the routes
 
 const listener = app.listen(process.env.PORT || port, () => {
     console.log('Your app is listening on port ' + listener.address().port)
