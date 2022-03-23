65adf90322f538f9ded5fd23fa5c836d670fd74f
antwonung <sjoberg_13@hotmail.com>
2022-03-23 23:49:59 +0100

    boss page

--- client/src/components/Boss/Bossdate.jsx
+++ client/src/components/Boss/Bossdate.jsx
@@ -7,6 +7,7 @@ export default function Bossdate() {
               <span className="input-group-text" id="basic-addon1">date</span>
               <input className='form-control' type="date" placeholder='Worked hours' id="date" name="worked-hours"></input>
           </div>
+
     </>
   )
 }
--- client/src/pages/Boss.jsx
+++ client/src/pages/Boss.jsx
@@ -1,14 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import NavbarComp from '../components/NavbarComp.jsx'
 import BossTable from '../components/Boss/BossTable.jsx';
-import BossDate from '../components/Boss/Bossdate.jsx';
-import { Container, Row, Col,  Button} from 'react-bootstrap'
+import { Container, Row, Col,Button,ButtonGroup} from 'react-bootstrap'
 
 
 export default class Boss extends Component {
+  constructor() {
+    
+    super();
+    this.state={
+      datWeek:"date"
+    }
+  }
+  updateDatWeek(value){
+    this.setState({datWeek:value})
+  }
+ 
   render() {
+   
+    let datWeek ="date";
     return (
         <>
+        
         <div className='boss-div'>
         <Container>
           <Row className='row-push-down'>
@@ -17,12 +30,20 @@ export default class Boss extends Component {
           <Row className='row-button-bossdate'>
             <Col className='col-title-day-view'><h2>Select date for day view</h2></Col>
             <Col>
-              <BossDate></BossDate>
+            <div className='input-group'>
+              <span className="input-group-text" id="basic-addon1">date</span>
+              <input className='form-control' type={this.state.datWeek} placeholder='Worked hours' id="date" name="worked-hours"></input>
+          </div>
             </Col>
             <Col>
               <Row>
                 <Col></Col>
-                <Col xs={6}><Button variant="primary" size="md">View day report</Button></Col>
+                <Col xs={6}> 
+                  <ButtonGroup size="lg">
+                    <Button onClick={() =>this.updateDatWeek("week")}>Weeks</Button>
+                    <Button onClick={() =>this.updateDatWeek("date")}>Day</Button>
+                  </ButtonGroup>
+                </Col>
                 <Col></Col>
               </Row>
             </Col>
@@ -39,4 +60,5 @@ export default class Boss extends Component {
         </>
     )
   }
+ 
 }
