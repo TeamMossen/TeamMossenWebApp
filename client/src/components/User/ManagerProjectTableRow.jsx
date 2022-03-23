feb3f86aade8f6ca7b08f3a7d94629f0bab2ac85
alexmar000 <alex.olave@inteheltfel.se>
2022-03-23 12:44:15 +0100

    ! warnings

--- client/src/components/User/ManagerProjectTableRow.jsx
+++ client/src/components/User/ManagerProjectTableRow.jsx
@@ -2,35 +2,27 @@ import React, {useEffect, useState} from 'react';
 
 export default function ManagerProjectTableRow(props) {
   console.log(props);
-  let title = props.proj.Projectname.title[0].plain_text;
+  let hoursLeft = props.proj["Hours left"].formula.number;
+  let timespan = `${props.proj.Timespan.date.start} - ${props.proj.Timespan.date.end}`
   let reportsOutsideTimespan = props.proj.ReportTimes.rollup.array;
-  const backgroundColor =
-    props.proj["Hours left"].formula.number < 0 ? "red" : "none";
-  const timeWarning = { backgroundColor: backgroundColor };
-  let outsideSpanWarning = { backgroundColor: "none" };
+  hoursLeft += hoursLeft < 0 ? " ❗️" : "";
   let modifyTitleWarning = props.proj["Hours left"].formula.number < 0;
 
   reportsOutsideTimespan.every((report) => {
     if (report.formula.string == "Fail") {
-      title += " ❗️";
-      outsideSpanWarning.backgroundColor = "red";
+      timespan += " ❗️";
       return;
     }
   });
 
   return (
     <tr>
-      <td id="projectTitle">{title}</td>
+      <td>{props.proj.Projectname.title[0].plain_text}</td>
       <td>{props.proj.Status.select.name}</td>
       <td>{props.proj.Hours.number}</td>
       <td>{props.proj["Worked hours"].rollup.number}</td>
-      <td style={timeWarning} id="hours-left">
-        {props.proj["Hours left"].formula.number}
-      </td>
-      <td style={outsideSpanWarning}>
-        {props.proj.Timespan.date.start} - {props.proj.Timespan.date.end}
-      </td>
-      {/* {document.getElementById("hours-left").innerHTML = hoursLeftTextWarning} */}
+      <td>{hoursLeft}</td>
+      <td>{timespan}</td>
     </tr>
   );
 }
