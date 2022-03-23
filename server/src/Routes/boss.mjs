8e9b97cc50afaf190070e5f266c8380c2a413efd
Kyemate <henke@mdpn.net>
2022-03-24 00:19:07 +0100

    skål notion

--- server/src/Routes/boss.mjs
+++ server/src/Routes/boss.mjs
@@ -1,7 +1,7 @@
 import express from 'express';
 export const bossRouter  = express.Router();
-import {   } from  '../Controllers/boss.mjs';
+import {  getWorkedHours } from  '../Controllers/boss.mjs';
 import { AuthenticateToken } from '../Controllers/login.mjs';
 
 //bossRouter.get('/data', returnDatabase);
-bossRouter.get('/boss/getWorkedHours', getProjects);
+bossRouter.get('/boss/getWorkedHours', getWorkedHours);
