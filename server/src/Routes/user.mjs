caf74d50f377d384bd927737beca6d26f52139b5
Kyemate <henke@mdpn.net>
2022-03-29 13:25:39 +0200

    skål notion

--- server/src/Routes/user.mjs
+++ server/src/Routes/user.mjs
@@ -6,12 +6,3 @@ import { AuthenticateToken } from '../Controllers/login.mjs';
 userRouter.get('/data', returnDatabase);
 userRouter.get('/user/getProjects', AuthenticateToken, getProjects);
 userRouter.post('/user/postTimeReport',postTimeReport);
-// userRouter.post('/tea', newTea);
-// userRouter.delete('/tea', deleteAllTea);
-
-// userRouter.get('/tea/:name', getOneTea);
-// userRouter.post('/tea/:name', newComment);
-// userRouter.delete('/tea/:name', deleteOneTea);
-
-// export  userRouter;
-
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,10 @@
 import express from 'express'; //import express
-//import { Client } from "@notionhq/client";
 import cors from 'cors';
 import { userRouter } from './Routes/user.mjs';
 import { loginRouter } from './Routes/login.mjs';
 import { bossRouter } from './Routes/boss.mjs';
 import { projectmanagerRouter } from './Routes/projectmanager.mjs';
-//const routes = require('./Routes/user'); // import the routes
 
-//const host = "localhost";
 const port = 8000;
 
 
@@ -21,37 +18,3 @@ app.use('/', userRouter, loginRouter, bossRouter, projectmanagerRouter); //to us
 const listener = app.listen(process.env.PORT || port, () => {
     console.log('Your app is listening on port ' + listener.address().port)
 })
-
-
-
-
-// import http from 'http';
-// import './Controllers/User.js';
-
-
-// const server = http.createServer(async (req, res) => {
-//   // Avoid CORS errors
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Content-Type", "application/json");
-//   switch (req.url) {
-//     // Will respond to queries to the domain root (like http://localhost/)
-//     case "/user":
-//       const response = await notion.databases.query({
-//         database_id: notionDatabaseId,
-//       });
-//       // Only supports the / route
-//       const query = response.results.filter(x => x.properties.Status.select.name == "Active").map(x => x.properties);
-//       console.log(query);
-//       res.writeHead(200);
-//       res.write(JSON.stringify(query));
-//       res.end();
-//       break;
-//     default:
-//       res.writeHead(401);
-//       res.end(JSON.stringify({ error: "Resource not found" }));
-//   }
-// });
-
-// server.listen(port, host, () => {
-//   console.log(`Server is running on http://${host}:${port}`);
-// });
