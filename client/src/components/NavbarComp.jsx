81dea832911dcc37f88e48e36f0d249a4226357f
alexmar000 <alex.olave@inteheltfel.se>
2022-03-29 14:41:22 +0200

    h

--- client/src/components/NavbarComp.jsx
+++ client/src/components/NavbarComp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom';
 import { Navbar, Nav, Container} from 'react-bootstrap'
-import logo from '../static/logo.svg';
+import logo from '../static/logo.png';
 
 
 export default function NavbarComp({userData, activePage}) {
@@ -14,16 +14,16 @@ export default function NavbarComp({userData, activePage}) {
                     height="30"
                     className="d-inline-block align-top"
                     alt="React Bootstrap logo" />
-                    App text
+                     Mössen
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav className="me-auto"> {/* my-2 my-lg-0 style={{ maxHeight: '100px' }} navbarScroll */}
                         <span className="nav-link" style={{cursor: "pointer"}} onClick={() => activePage("user")}>Dashboard</span>
-                        {(userData.role == "ProjectManager" || userData.role == "Boss") && <>
+                        {/* {(userData.role == "ProjectManager" || userData.role == "Boss") && <>
                             <NavLink className="nav-link" to="/pmpage1">PM page 1</NavLink>
                             </>
-                        }
+                        } */}
                         {userData.role == "Boss" &&
                             <span className="nav-link" style={{cursor: "pointer"}} onClick={() => activePage("boss")}>Summed Hours</span>
                             //<NavLink className="nav-link" to="/boss">Boss page</NavLink>
